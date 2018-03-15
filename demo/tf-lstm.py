0706a03fa9facefefcd8383370ee6376e0d91644
hzzhujiajun <hzzhujiajun@corp.netease.com>
2018-03-15 14:26:51 +0800

    aaa

---
+++ demo/tf-lstm.py
@@ -0,0 +1,186 @@
+import numpy as np
+import tensorflow as tf
+import re
+import matplotlib.pyplot as plt
+from os import listdir
+from os.path import isfile, join
+wordsList = np.load('wordsList.npy')
+print('Loaded the word list!')
+wordsList = wordsList.tolist()  # Originally loaded as numpy array
+wordsList = [word.decode('UTF-8') for word in wordsList]  # Encode words as UTF-8
+wordVectors = np.load('wordVectors.npy')
+print('Loaded the word vectors!')
+print(len(wordsList))
+print(wordVectors.shape)
+baseballIndex = wordsList.index('baseball')
+
+maxSeqLength = 10 #Maximum length of sentence
+numDimensions = 300 #Dimensions for each word vector
+firstSentence = np.zeros((maxSeqLength), dtype='int32')
+firstSentence[0] = wordsList.index("i")
+firstSentence[1] = wordsList.index("thought")
+firstSentence[2] = wordsList.index("the")
+firstSentence[3] = wordsList.index("movie")
+firstSentence[4] = wordsList.index("was")
+firstSentence[5] = wordsList.index("incredible")
+firstSentence[6] = wordsList.index("and")
+firstSentence[7] = wordsList.index("inspiring")
+# firstSentence[8] and firstSentence[9] are going to be 0
+print(firstSentence.shape)
+print(firstSentence) # Shows the row index for each word
+with tf.Session() as sess:
+    print(tf.nn.embedding_lookup(wordVectors,firstSentence).eval().shape)
+
+positiveFiles = ['positiveReviews/' + f for f in listdir('positiveReviews/') if isfile(join('positiveReviews/', f))]
+negativeFiles = ['negativeReviews/' + f for f in listdir('negativeReviews/') if isfile(join('negativeReviews/', f))]
+numWords = []
+for pf in positiveFiles:
+    with open(pf, "r", encoding='utf-8') as f:
+        line=f.readline()
+        counter = len(line.split())
+        numWords.append(counter)
+print('Positive files finished')
+
+for nf in negativeFiles:
+    with open(nf, "r", encoding='utf-8') as f:
+        line=f.readline()
+        counter = len(line.split())
+        numWords.append(counter)
+print('Negative files finished')
+
+numFiles = len(numWords)
+print('The total number of files is', numFiles)
+print('The total number of words in the files is', sum(numWords))
+print('The average number of words in the files is', sum(numWords)/len(numWords))
+
+plt.hist(numWords, 50)
+plt.xlabel('Sequence Length')
+plt.ylabel('Frequency')
+plt.axis([0, 1200, 0, 8000])
+plt.show()
+
+maxSeqLength = 250
+fname = positiveFiles[3] # Can use any valid index (not just 3)
+with open(fname) as f:
+    for lines in f:
+        print(lines)
+
+strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
+
+
+def variable_summaries(var, name):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope(name):
+        # 计算参数的均值，并使用tf.summary.scaler记录
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+
+        # 计算参数的标准差
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        # 使用tf.summary.scaler记录记录下标准差，最大值，最小值
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        # 用直方图记录参数的分布
+        tf.summary.histogram('histogram', var)
+
+
+def cleanSentences(string):
+    string = string.lower().replace("<br />", " ")
+    return re.sub(strip_special_chars, "", string.lower())
+firstFile = np.zeros((maxSeqLength), dtype='int32')
+with open(fname) as f:
+    indexCounter = 0
+    line = f.readline()
+    cleanedLine = cleanSentences(line)
+    split = cleanedLine.split()
+    for word in split:
+        try:
+            firstFile[indexCounter] = wordsList.index(word)
+        except ValueError:
+            firstFile[indexCounter] = 399999 #Vector for unknown words
+        indexCounter = indexCounter + 1
+ids = np.load('idsMatrix.npy')
+from random import randint
+batchSize = 24
+lstmUnits = 64
+numClasses = 2
+iterations = 100000
+def getTrainBatch():
+    labels = []
+    arr = np.zeros([batchSize, maxSeqLength])
+    for i in range(batchSize):
+        if (i % 2 == 0):
+            num = randint(1, 11499)
+            labels.append([1, 0])
+        else:
+            num = randint(13499, 24999)
+            labels.append([0, 1])
+        arr[i] = ids[num-1:num]
+    return arr, labels
+
+def getTestBatch():
+    labels = []
+    arr = np.zeros([batchSize, maxSeqLength])
+    for i in range(batchSize):
+        num = randint(11499,13499)
+        if (num <= 12499):
+            labels.append([1,0])
+        else:
+            labels.append([0,1])
+        arr[i] = ids[num-1:num]
+    return arr, labels
+
+
+tf.reset_default_graph()
+
+labels = tf.placeholder(tf.float32, [batchSize, numClasses])
+input_data = tf.placeholder(tf.int32, [batchSize, maxSeqLength])
+data = tf.Variable(tf.zeros([batchSize, maxSeqLength, numDimensions]), dtype=tf.float32)
+data = tf.nn.embedding_lookup(wordVectors, input_data)
+lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
+lstmCell = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=0.75)
+value, _ = tf.nn.dynamic_rnn(lstmCell, data, dtype=tf.float32)
+
+weight = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]))
+variable_summaries(weight, 'weight')
+
+bias = tf.Variable(tf.constant(0.1, shape=[numClasses]))
+variable_summaries(bias, 'bias')
+
+value = tf.transpose(value, [1, 0, 2])
+last = tf.gather(value, int(value.get_shape()[0]) - 1)
+prediction = (tf.matmul(last, weight) + bias)
+tf.summary.histogram('outputs', prediction)
+
+correctPred = tf.equal(tf.argmax(prediction, 1), tf.argmax(labels, 1))
+accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels))
+optimizer = tf.train.AdamOptimizer().minimize(loss)
+import datetime
+
+tf.summary.scalar('Loss', loss)
+tf.summary.scalar('Accuracy', accuracy)
+merged = tf.summary.merge_all()
+logdir = "./tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
+writer = tf.summary.FileWriter(logdir, sess.graph)
+sess = tf.InteractiveSession()
+saver = tf.train.Saver()
+sess.run(tf.global_variables_initializer())
+
+for i in range(iterations):
+   #Next Batch of reviews
+   nextBatch, nextBatchLabels = getTrainBatch()
+   sess.run(optimizer, {input_data: nextBatch, labels: nextBatchLabels})
+
+   #Write summary to Tensorboard
+   if (i % 50 == 0):
+       summary = sess.run(merged, {input_data: nextBatch, labels: nextBatchLabels})
+       writer.add_summary(summary, i)
+
+   #Save the network every 10,000 training iterations
+   if (i % 10000 == 0 and i != 0):
+       save_path = saver.save(sess, "models/pretrained_lstm.ckpt", global_step=i)
+       print("saved to %s" % save_path)
+writer.close()
